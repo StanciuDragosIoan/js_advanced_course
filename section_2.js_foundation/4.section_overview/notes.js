2dc7e1e6562855146ac07caf7207112fe9bfd0a3
Dragos <Dragos.Dragos@honeywell.com>
2019-10-23 10:56:11 +0300

    test

--- section_2.js_foundation/4.section_overview/notes.js
+++ section_2.js_foundation/4.section_overview/notes.js
@@ -1,6 +1,6 @@
 /*
     will build the foundation for JS and any language
 
-    will cover: js engine, js runtime, interpreter/compiler/jit ocmpiler,
+    will cover: js engine, js runtime, interpreter/compiler/jit compiler,
     call stack, memory heaps, stackoverflow and memory leaks, node.js, single threaded model..
 */
