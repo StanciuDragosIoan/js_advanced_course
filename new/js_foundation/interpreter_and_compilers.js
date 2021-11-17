8cd2de3e4c94452bf989119447a298aa19d9c4cd
dragos.stanciu <dragos.stanciu@tremend.com>
2021-11-17 15:01:45 +0200

    test

---
+++ new/js_foundation/interpreter_and_compilers.js
@@ -0,0 +1,9 @@
+/*
+
+interpreter = translates and reads the files line by line on the fly
+interpreters are quick to set up and run (much faster than a compiler)
+
+compiler = compiles code into lower lvl code (like machine code)
+compilers are slower to set up but they optimize the execution (for instance inside of a loop)
+
+*/
---
+++ new/js_foundation/js_engine.js
@@ -0,0 +1,19 @@
+/*
+    JS Engine
+
+    The engine transforms our JS code into machine code which can be understood and executed by the PC
+
+    in 2008 google created V8 engine for their google maps api
+
+    first JS engine was created by Brendan Eich (JS creator)
+
+
+    Inside the JS Engine:
+
+
+    engine gets JS file
+
+
+    ECMAScript engines (ES standards that engines have to respect while creating)
+
+*/
